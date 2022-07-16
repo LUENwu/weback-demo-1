c4ef2fa1752616b1ba23dabebfe2dd35d12856b2
LUENwu <1937839754@qq.com>
2022-07-16 13:43:48 +0800

    version-1

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path')
+
+ module.exports = {
+  mode: 'production',
+  entry:'./src/index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  }
+}
